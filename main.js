9fd77c57ff5ef693016ebe0afd03a33d83377d6f
amine <aminmohamedaboamasha@gmail.com>
2024-11-20 00:00:12 +0100

    Fixed a bug in the login system

---
+++ main.js
@@ -0,0 +1,7 @@
+// دالة لفتح وإغلاق القائمة الفرعية عند النقر
+function toggleMenu() {
+    const menu = document.querySelector('.list2');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+}
+
+
